866a270f5fe5f239dc985d0ab89b83eac0791656
alinelsilva <aline-lima-silva@hotmail.com>
2020-09-17 11:23:30 -0300

    Desafio Stoom

---
+++ AddressStoom/src/main/java/br/com/stomm/address/AddressStoomApplication.java
@@ -0,0 +1,13 @@
+package br.com.stomm.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressStoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressStoomApplication.class, args);
+	}
+
+}
---
+++ AddressStoom/src/test/java/br/com/stomm/address/AddressStoomApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stomm.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressStoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
